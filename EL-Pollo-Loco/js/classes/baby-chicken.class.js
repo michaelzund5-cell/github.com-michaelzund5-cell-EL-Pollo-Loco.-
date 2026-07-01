69d9c2901125ba9e7c04bd30cb56ded668102a3e
michaelzund5-cell <michaelzund5@gmail.com>
2026-07-01 11:51:31 +0200

    Add baby chicken and improve chicken movement

--- EL-Pollo-Loco/js/classes/baby-chicken.class.js
+++ EL-Pollo-Loco/js/classes/baby-chicken.class.js
@@ -3,8 +3,12 @@ class BabyChicken extends Chicken {
     constructor() {
         super();
 
+        // Spezifische Eigenschaften für Baby Chicken
+        this.loadImage("./assets/img/img_pollo_locco/img/3_enemies_chicken/chicken_small/1_walk/1_w.png");
+
         this.width = 60;
         this.height = 60;
+        this.y = 380;
+        this.speed = 0.1 + Math.random() * 0.3;
     }
-
 }
--- EL-Pollo-Loco/js/classes/chicken.class.js
+++ EL-Pollo-Loco/js/classes/chicken.class.js
@@ -1,17 +1,36 @@
 class Chicken extends MoveableObject {
 
+
+    IMAGES_WALKING = [
+        "./assets/img/img_pollo_locco/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png",
+        "./assets/img/img_pollo_locco/img/3_enemies_chicken/chicken_normal/1_walk/2_w.png",
+        "./assets/img/img_pollo_locco/img/3_enemies_chicken/chicken_normal/1_walk/3_w.png"
+    ];
+
+
     constructor() {
         super();
 
-        this.loadImage("./assets/img/img_pollo_locco/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png");
+        this.loadImage(this.IMAGES_WALKING[0]);
 
-        this.x = 500;
+        this.x = 500 + Math.random() * 500;
         this.y = 360;
         this.width = 80;
         this.height = 80;
+
+        this.speed = 0.15 + Math.random() * 0.5;
+
+        this.animate();
     }
 
     animate() {
-
+        setInterval(() => {
+            this.moveLeft();
+        }, 1000 / 60);
     }
+
+    moveLeft() {
+        this.x -= this.speed;
+    }
+
 }
