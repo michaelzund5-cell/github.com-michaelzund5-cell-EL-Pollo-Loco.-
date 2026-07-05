5131d83f676db89c7b0e67e16210adec07ab186f
michaelzund5-cell <michaelzund5@gmail.com>
2026-07-05 11:43:29 +0200

    Fix baby chicken animation paths

--- EL-Pollo-Loco/js/classes/baby-chicken.class.js
+++ EL-Pollo-Loco/js/classes/baby-chicken.class.js
@@ -1,14 +1,26 @@
 class BabyChicken extends Chicken {
 
+
+    IMAGES_WALKING = [
+        "./assets/img/img_pollo_locco/img/3_enemies_chicken/chicken_small/1_walk/1_w.png",
+        "./assets/img/img_pollo_locco/img/3_enemies_chicken/chicken_small/1_walk/2_w.png",
+        "./assets/img/img_pollo_locco/img/3_enemies_chicken/chicken_small/1_walk/3_w.png"
+    ];
+
+
+
     constructor() {
         super();
 
-        // Spezifische Eigenschaften für Baby Chicken
-        this.loadImage("./assets/img/img_pollo_locco/img/3_enemies_chicken/chicken_small/1_walk/1_w.png");
+        this.loadImage(this.IMAGES_WALKING[0]);
+        this.loadImages(this.IMAGES_WALKING);
 
         this.width = 60;
         this.height = 60;
         this.y = 380;
         this.speed = 0.1 + Math.random() * 0.3;
     }
+
+
 }
+
